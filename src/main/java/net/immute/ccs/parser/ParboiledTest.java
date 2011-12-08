80ed4518f7d7f33af98748ea533441d12d5f781e
Matt Hellige <matt@immute.net>
2011-12-07 23:37:14 -0600

    More hacking.

--- src/main/java/net/immute/ccs/parser/ParboiledTest.java
+++ src/main/java/net/immute/ccs/parser/ParboiledTest.java
@@ -4,6 +4,7 @@ import org.parboiled.BaseParser;
 import org.parboiled.Parboiled;
 import org.parboiled.Rule;
 import org.parboiled.annotations.BuildParseTree;
+import org.parboiled.annotations.SuppressSubnodes;
 import org.parboiled.parserunners.ReportingParseRunner;
 import org.parboiled.support.ParseTreeUtils;
 import org.parboiled.support.ParsingResult;
@@ -11,6 +12,7 @@ import org.parboiled.support.ParsingResult;
 public class ParboiledTest {
     @BuildParseTree
     static class CcsParser extends BaseParser<Void> {
+        @SuppressSubnodes
         Rule nl() {
             return FirstOf('\n', Sequence('\r', Optional('\n')));
         }
@@ -19,24 +21,40 @@ public class ParboiledTest {
             return FirstOf(AnyOf(" \t\f"), nl());
         }
 
+        @SuppressSubnodes
         Rule sp() {
             return ZeroOrMore(FirstOf(blockComment(),
                     Sequence("//", ZeroOrMore(NoneOf("\n\r"), FirstOf(nl(), EOI))),
                     space()));
         }
-        
+
+        @SuppressSubnodes
         Rule blockComment() {
             return Sequence("/*", ZeroOrMore(FirstOf(blockComment(), Sequence(TestNot("*/"), ANY))), "*/");
         }
 
+        @SuppressSubnodes
         Rule string() {
             return FirstOf(
                     Sequence('\'', ZeroOrMore(NoneOf("\'\n\r")), '\''),
                     Sequence('\"', ZeroOrMore(NoneOf("\"\n\r")), '\"'));
         }
 
+        Rule val() {
+            return string(); // TODO hex, long, double
+        }
+
+        Rule identChar() {
+            return FirstOf(CharRange('A', 'Z'), CharRange('a', 'z'), CharRange('0', '9'), '$', '_');
+        }
+
+        @SuppressSubnodes
+        Rule ident() {
+            return FirstOf(OneOrMore(identChar()), string());
+        }
+
         Rule property() {
-            return String("prop"); // TODO
+            return Sequence(Optional("inherit"), sp(), ident(), sp(), '=', sp(), val(), sp()); //qi::lexeme[val >> !ident];
         }
         
         Rule selector() {
@@ -54,7 +72,7 @@ public class ParboiledTest {
 
         Rule rule() {
             return Sequence(FirstOf(imprt(), property(), Sequence(selector(), sp(), '{', sp(),
-                    ZeroOrMore(rule()), sp(), '}')), sp(), Optional(';'), sp());
+                    ZeroOrMore(rule()), '}')), sp(), Optional(';'), sp());
         }
 
         Rule ruleset() {
@@ -63,7 +81,8 @@ public class ParboiledTest {
     }
 
     public static void main(String[] args) {
-        String input = args.length > 0 ? args[0] : "\n@context (selector);\n @import /* hi */ 'foo';\n@import 'bar';";
+        String input = args.length > 0 ? args[0] : "\n@context (selector);\n @import /* hi */ 'foo';\n@import 'bar';" +
+                "selector { foo = 'test' };";
         CcsParser parser = Parboiled.createParser(CcsParser.class);
         ParsingResult<?> result = new ReportingParseRunner(parser.ruleset()).run(input);
         String parseTreePrintOut = ParseTreeUtils.printNodeTree(result);
